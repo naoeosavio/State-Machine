3f3b64cc5b8efbaa3ac8a5f6e42d44f4a6037ad2
nao e o savio <savioscmendes@gmail.com>
2025-07-21 22:46:04 -0300

    feat: export run function from main

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@ export { new_mach } from './main';
 export { time_to_tick } from './main';
 export { register_action } from './main';
 export { compute } from './main';
+export { run } from './main';
